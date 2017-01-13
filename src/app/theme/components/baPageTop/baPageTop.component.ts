e4d7bb3d7ccde08e4ea41019801cdd9bc4607051
minh.nguyen <minhnb89@gmail.com>
2017-01-13 11:54:38 +0700

    [PP-28] Implement logout function

--- src/app/theme/components/baPageTop/baPageTop.component.ts
+++ src/app/theme/components/baPageTop/baPageTop.component.ts
@@ -1,31 +1,51 @@
-import {Component, ViewEncapsulation} from '@angular/core';
+import { Component, ViewEncapsulation, Injector } from '@angular/core';
 
-import {GlobalState} from '../../../global.state';
+import { GlobalState } from '../../../global.state';
+
+import { ZapppBaseComponent } from '../../../zappp/baseComponent/base.component';
+import { UserService } from '../../../services/user';
 
 @Component({
-  selector: 'ba-page-top',
-  styles: [require('./baPageTop.scss')],
-  template: require('./baPageTop.html'),
-  encapsulation: ViewEncapsulation.None
+	selector: 'ba-page-top',
+	styles: [require('./baPageTop.scss')],
+	template: require('./baPageTop.html'),
+	encapsulation: ViewEncapsulation.None,
+	providers: [UserService]
 })
-export class BaPageTop {
+export class BaPageTop extends ZapppBaseComponent {
 
-  public isScrolled:boolean = false;
-  public isMenuCollapsed:boolean = false;
+	public isScrolled: boolean = false;
+	public isMenuCollapsed: boolean = false;
 
-  constructor(private _state:GlobalState) {
-    this._state.subscribe('menu.isCollapsed', (isCollapsed) => {
-      this.isMenuCollapsed = isCollapsed;
-    });
-  }
+	constructor(private injector: Injector, private _state: GlobalState, private userService: UserService) {
+        super(injector);
+		this._state.subscribe('menu.isCollapsed', (isCollapsed) => {
+			this.isMenuCollapsed = isCollapsed;
+		});
+	}
 
-  public toggleMenu() {
-    this.isMenuCollapsed = !this.isMenuCollapsed;
-    this._state.notifyDataChanged('menu.isCollapsed', this.isMenuCollapsed);
-    return false;
-  }
+	public toggleMenu() {
+		this.isMenuCollapsed = !this.isMenuCollapsed;
+		this._state.notifyDataChanged('menu.isCollapsed', this.isMenuCollapsed);
+		return false;
+	}
 
-  public scrolledChanged(isScrolled) {
-    this.isScrolled = isScrolled;
-  }
+	public scrolledChanged(isScrolled) {
+		this.isScrolled = isScrolled;
+	}
+
+	public signOut(event) {
+        this.userService.logOut().subscribe(
+            res => {
+                this.goToLoginPage();
+            },
+            error => {
+                this.goToLoginPage();
+            });;
+        event.preventDefault();
+	}
+
+    goToLoginPage() {
+        this.router.navigateByUrl('/login');
+    }
 }
