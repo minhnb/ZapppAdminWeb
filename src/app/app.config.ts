5fc9c1719ce537570892aa951f7aa7aefa36143e
minh.nguyen <minhnb89@gmail.com>
2017-05-15 14:11:17 +0700

    Remove comment row

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,5 @@
 export const AppConfig = {
-    // API_URL: 'http://dev.dip.cvp.io/',
     API_URL: 'https://dev-api.zappp.com/',
-    // FB_APP_ID: "1656233541263739",
     FB_APP_ID: "708122015999736",
     FB_SECRET_ID: "8ea337dbd84699a10d75daa30093e5dc",
     GOOGLE_APP_CLIENT_ID: '887424921355-hb74akt3p29labk66u6l11pkh1g8cgs7.apps.googleusercontent.com',
